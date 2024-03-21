54a92427ab637226dadd7f89608b2ba9ad8ef66c
Mobmab_Lee <mobmabpiyarat@gmail.com>
2024-03-22 04:14:16 +0700

    update

--- src/component/pages/Navbar/Navbar.js
+++ src/component/pages/Navbar/Navbar.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { FaBars } from 'react-icons/fa';
+import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
+  const [Mobile, setMobile] = useState(true)
+
+
   return (
     <nav className='navbar'>
         <div className='container'>
             <h3 className='logo'> Logo </h3>
 
-            <ul className='nav-links'>
+            <ul className={Mobile? 'nav-links-mobile': 'nav-links'} onClick={() => setMobile(false)}>
                 <Link to='/'> <li>Home</li></Link>
                 <Link to='/tutorial'> <li>Tutorial</li></Link>
                 <Link to='/dashboard'> <li>Dashboard</li></Link>
             </ul>
+            <button className='mobile-menu-icon'>
+                {Mobile? <ImCross/>: <FaBars /> }
+            </button>
         </div>
     </nav>
   )
